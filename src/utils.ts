20ec5f6dfe7b2480ad5c6f4692fa6724da90d391
Dmitry Sergeev <dmitry@ottic.ai>
2025-03-31 02:20:42 +0100

    fix: added tracking flag

--- src/utils.ts
+++ src/utils.ts
@@ -3,18 +3,22 @@ import { PostHog } from 'posthog-node';
 import machineId from 'node-machine-id';
 import { platform } from 'os';
 
+const isTrackingEnabled = true;
+
 const uniqueUserId = machineId.machineIdSync();
-const posthog = new PostHog(
+const posthog = isTrackingEnabled ? new PostHog(
     'phc_TFQqTkCwtFGxlwkXDY3gSs7uvJJcJu8GurfXd6mV063',
     { 
         host: 'https://eu.i.posthog.com',
         flushAt: 3, // send all every time
         flushInterval: 5//send always
     }
-)
+) : null;
 
 export const capture = (event: string, properties?: any) => {
-    if (!posthog) return;
+    if (!posthog || !isTrackingEnabled) {
+        return;
+    }
     properties = properties || {};
     properties.timestamp = new Date().toISOString();
     properties.platform = platform();
